39985c10dda408077f943578020195e1bcda43eb
Sam Osekeny <samuelpo49@gmail.com>
2021-10-16 19:25:53 -0600

    "Added my name as sam and printed a statement about it"

--- src/costigan/main.java
+++ src/costigan/main.java
@@ -3,5 +3,6 @@ package costigan;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Steve");
+        System.out.println("Hello Steve, my name is Sam.");
     }
 }
