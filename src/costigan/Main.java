3a35dcb29f53177e258f6dd49d55b28282aff622
Richard Vizarreta <vizarretari27@byui.edu>
2021-10-16 21:54:10 -0600

    Greetings, again

--- src/costigan/Main.java
+++ src/costigan/Main.java
@@ -1,8 +1,9 @@
 package costigan;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Steve");
         System.out.println("Hello Steve, my name is Sam.");
+        System.out.println("Hello Steve, my name is Richard.");
     }
 }
